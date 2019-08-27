16e30ae43a73b8b548f0d664f6159e21678c3e23
Steven Massaro <steven.massaro.web@gmail.com>
2019-08-27 16:42:26 -0400

    specify music location through properties

--- src/main/java/music/service/FileService.java
+++ src/main/java/music/service/FileService.java
@@ -6,6 +6,7 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.TagException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -16,9 +17,12 @@ import java.util.Collection;
 @Service
 public class FileService {
 
+    @Value("${music.file.source}")
+    private String musicFileSource;
+
     public Collection<File> listMusicFiles() throws ReadOnlyFileException, IOException, TagException, InvalidAudioFrameException, CannotReadException {
 //        return FileUtils.listFiles(new File("/music"), new String[]{"mp3", "flac", "FLAC"}, true);
-        return FileUtils.listFiles(new File("/music"), new String[]{"mp3", "flac", "FLAC"}, true);
+        return FileUtils.listFiles(new File(musicFileSource), new String[]{"mp3", "flac", "FLAC"}, true);
     }
 
     public File getFile() throws ReadOnlyFileException, CannotReadException, TagException, InvalidAudioFrameException, IOException {
