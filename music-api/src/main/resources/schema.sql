0d01bd9d0a4f55ab928a77ab3ef087085e5074d9
Steven Massaro <steven.massaro.web@gmail.com>
2019-09-24 21:45:51 -0400

    add referential integrity to track and plays tables

--- music-api/src/main/resources/schema.sql
+++ music-api/src/main/resources/schema.sql
@@ -5,7 +5,7 @@ create schema music;
 
 drop table if exists music.track;
 create table music.track (
-  id serial not null,
+  id serial not null unique,
   title varchar(1000) not null,
   location varchar primary key,
   hash varchar(128) not null,
@@ -26,6 +26,6 @@ create table music.track (
 drop table if exists music.plays;
 create table music.plays (
   id serial not null,
-  songid int not null,
+  songid int not null references music.track(id),
   playdate timestamp not null
 );
