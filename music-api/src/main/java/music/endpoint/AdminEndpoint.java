496af8fa4d21a4c78901e22140b8ec0d4370b89e
Steven Massaro <steven.massaro.web@gmail.com>
2019-10-01 16:20:29 -0400

    change dbsync endpoint to post

--- music-api/src/main/java/music/endpoint/AdminEndpoint.java
+++ music-api/src/main/java/music/endpoint/AdminEndpoint.java
@@ -11,10 +11,7 @@ import org.jaudiotagger.tag.TagException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -35,7 +32,7 @@ public class AdminEndpoint {
         this.trackService = trackService;
     }
 
-    @GetMapping("/dbSync")
+    @PostMapping("/dbSync")
     public List<Track> syncTracksToDb() throws ReadOnlyFileException, CannotReadException, TagException, InvalidAudioFrameException, IOException {
         logger.info("Begin database sync");
         List<Track> files = metadataService.getTracks();
