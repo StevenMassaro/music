4c4c4423955b19cb896dad2593036ba6480abf8c
Steven Massaro <steven.massaro.web@gmail.com>
2020-09-23 15:26:32 -0400

    CORS: allow all origins on all paths

--- music-api/src/main/java/music/App.java
+++ music-api/src/main/java/music/App.java
@@ -3,6 +3,9 @@ package music;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App extends SpringBootServletInitializer {
@@ -10,4 +13,14 @@ public class App extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
 }
